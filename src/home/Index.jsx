55ca316f7487fc951b6128eae35f2becad2b8640
Carlos Wilson <carlos.wilson@einfochips.com>
2022-07-19 14:10:11 -0500

    Editing app description

--- src/home/Index.jsx
+++ src/home/Index.jsx
@@ -8,7 +8,7 @@ const Home = () => {
 
     return (
         <div>
-            <h1>React - CRUD Example with Redux & Formik</h1>
+            <h1>user-form App with Redux & Formik</h1>
             <button className="btn btn-primary" onClick={() => navigate(`/users`)}>
                 Manage Users
             </button>
